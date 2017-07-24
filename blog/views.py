97e174e8658e7eecfb93a61ae41d5c0f0e0a6084
Lucas <dalmolinlucas10@gmail.com>
2017-07-24 19:20:33 -0300

    Nova pagina HTML

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
-
 # Create your views here.
 def post_list(request):
-
-    return render(request, 'post_list.html', {})
+    posts_publicados = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
+    return render(request, 'post_list.html', {'posts': posts_publicados})
