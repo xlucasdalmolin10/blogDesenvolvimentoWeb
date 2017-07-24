4d6614a3513c83324fdf68d4b5ebc9391b8bd039
Lucas <dalmolinlucas10@gmail.com>
2017-07-24 15:18:35 -0300

    Teste

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
+from __future__ import unicode_literals
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+admin.site.register(Post)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
+from __future__ import unicode_literals
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
+def post_list(request):
+
+    return render(request, 'post_list.html', {})
